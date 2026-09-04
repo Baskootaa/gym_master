2af662a73c576c928b773ae084a196bfd23ab622
Baskootaa <mazen01289elbasyouny@gmail.com>
2026-09-04 19:01:41 +0300

    Use environment variables for database connection

--- config/db.php
+++ config/db.php
@@ -14,11 +14,11 @@ if (isset($conn) && $conn instanceof mysqli && isset($pdo) && $pdo instanceof PD
     return;
 }
 
-// قراءة بيانات الاتصال المباشرة لـ Railway بدون استخدام root محلياً على السيرفر
+// قراءة بيانات الاتصال مباشرة من متغيرات البيئة على Render
 $host = getenv('DB_HOST') ?: 'yamanote.proxy.rlwy.net';
 $port = getenv('DB_PORT') ?: '50569';
 $dbname = getenv('DB_DATABASE') ?: 'railway';
-$username = getenv('DB_USERNAME') ?: 'railway'; // تم تعديل اليوزر ليتوافق مع السيرفر
+$username = getenv('DB_USERNAME') ?: 'railway';
 $password = getenv('DB_PASSWORD') ?: 'FEUxYixXuaqMLNnlpGZnYEiOtCxMBVq';
 
 // 1. الاتصال باستخدام MySQLi
